b94deaec6c8d930974f0d69cc6b45e7aff63bd1c
jrb <>
2011-01-28 21:11:19 +0000

    propagate fixes, enhancements from main branch

--- site_scons/site_tools/registerTargets.py
+++ site_scons/site_tools/registerTargets.py
@@ -1,4 +1,4 @@
-#  $Id: $
+#  $Id: registerTargets.py,v 1.15 2011/01/28 01:05:37 jrb Exp $
 import os, pprint, sys
 from SCons.Script import *
 from fermidebug import fdebug
@@ -125,7 +125,7 @@ def generate(env, **kw):
             exes = []
             for bin in binaries:
                 if env['PLATFORM'] == 'win32':
-                    if  (str(bin.path)).find("manifest") != -1:
+                    if  (str(bin.path)).find("manifest") == -1:
                         exes.append(bin)
                 else:
                     exes.append(bin)
